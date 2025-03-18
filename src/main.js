a421165622a1d5daf4c5f583cd1c70c7768b0bfe
M-Lenvik <lenvik.marie@gmail.com>
2025-03-18 09:21:29 +0100

    added scss imports

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import './normalize.scss';
 import "./style.scss";
+import "./_mobile.scss";
+
 //import javascriptLogo from "./javascript.svg";
 //import viteLogo from "/vite.svg";
 import { setupCounter } from "./counter.js";
